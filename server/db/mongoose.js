a309f59faa9c6d9c829ec33f3d2fe29aba559e81
Chris Chuang <chrischuang0421@gmail.com>
2018-01-10 13:52:42 +0800

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
+const dbuser = 'chrischuang';
+const dbpassword = 'cxc421';
+const mongodburi = process.env.PORT
+  ? `mongodb://${dbuser}:${dbpassword}@ds249727.mlab.com:49727/todo-api`
+  : 'mongodb://localhost:27017/TodoApp';
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(mongodburi);
 
 module.exports = { mongoose };
