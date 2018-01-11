bd2169a6208df222e1caaaf80b2269b6cedfebe1
Chris Chuang <chrischuang0421@gmail.com>
2018-01-11 14:31:19 +0800

    Refine heroku database url

--- server/config/config.js
+++ server/config/config.js
@@ -7,10 +7,4 @@ if (env === 'development') {
 } else if (env === 'test') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-} else {
-  if (!process.env.MONGODB_URI) {
-    const dbuser = 'chrischuang';
-    const dbpassword = 'cxc421';
-    process.env.MONGODB_URI = `mongodb://${dbuser}:${dbpassword}@ds249727.mlab.com:49727/todo-api`;
-  }
-}
+} 
